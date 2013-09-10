b48f4d56dc88717a32b9d0d0c3988d1afe24987b
UENISHI Kota <kuenishi@gmail.com>
2013-09-11 01:11:24 +0900

    update copyright notice

--- test/bench_tests.erl
+++ test/bench_tests.erl
@@ -1,11 +1,22 @@
-%%%-------------------------------------------------------------------
-%%% @author UENISHI Kota <kuenishi@gmail.com>
-%%% @copyright (C) 2013, UENISHI Kota
-%%% @doc
-%%%
-%%% @end
-%%% Created : 18 Feb 2013 by UENISHI Kota <kuenishi@gmail.com>
-%%%-------------------------------------------------------------------
+%%
+%% MessagePack for Erlang
+%%
+%% Copyright (C) 2009-2013 UENISHI Kota
+%%
+%%    Licensed under the Apache License, Version 2.0 (the "License");
+%%    you may not use this file except in compliance with the License.
+%%    You may obtain a copy of the License at
+%%
+%%        http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%    Unless required by applicable law or agreed to in writing, software
+%%    distributed under the License is distributed on an "AS IS" BASIS,
+%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%    See the License for the specific language governing permissions and
+%%    limitations under the License.
+%%
+%% Created : 18 Feb 2013 by UENISHI Kota <kuenishi@gmail.com>
+
 -module(bench_tests).
 
 -ifdef(TEST).
--- test/msgpack_test.erl
+++ test/msgpack_test.erl
@@ -1,11 +1,22 @@
-%%%-------------------------------------------------------------------
-%%% @author UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
-%%% @copyright (C) 2011, UENISHI Kota
-%%% @doc
-%%%
-%%% @end
-%%% Created : 26 Apr 2011 by UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
-%%%-------------------------------------------------------------------
+%%
+%% MessagePack for Erlang
+%%
+%% Copyright (C) 2009-2013 UENISHI Kota
+%%
+%%    Licensed under the Apache License, Version 2.0 (the "License");
+%%    you may not use this file except in compliance with the License.
+%%    You may obtain a copy of the License at
+%%
+%%        http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%    Unless required by applicable law or agreed to in writing, software
+%%    distributed under the License is distributed on an "AS IS" BASIS,
+%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%    See the License for the specific language governing permissions and
+%%    limitations under the License.
+%%
+%% Created : 26 Apr 2011 by UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
+
 -module(msgpack_test).
 
 -include_lib("eunit/include/eunit.hrl").
