f02c3d5cc012d26257c9f91efbedee9c27269527
UENISHI Kota <kuenishi@gmail.com>
2023-09-12 21:24:42 +0900

    Wip

--- test/prop_msgpack.erl
+++ test/prop_msgpack.erl
@@ -12,7 +12,7 @@ prop_test() ->
         end).
 
 prop_pack() ->
-    ?FORALL(Term, msgpack_object(),
+    ?FORALL(Term, msgpack_term(),
         begin
             Bin = msgpack:pack(Term),
             is_binary(Bin)
@@ -26,6 +26,18 @@ boolean(_) -> true.
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
 %%%%%%%%%%%%%%%%%%
-msgpack_object() -> term().
+msgpack_term() ->
+    frequency(
+      [
+       {10, integer()},
+       {10, float()},
+       {10, boolean()},
+       {10, binary()},
+       {10, utf8()},
+       {2, ?LAZY(list(msgpack_term()))},
+       {1, ?LAZY(map(msgpack_term(), msgpack_term()))}
+      ]).
+
+
 mytype() ->
     term().
